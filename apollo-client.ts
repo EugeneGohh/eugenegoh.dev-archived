71aec9b3e6ce10773e81d16fd5ade6ad82532bb8
Eugene <gzkgt11@gmail.com>
2021-10-20 08:44:35 +0800

    configure apollo client to allow two graph

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,14 +1,28 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloLink,
+  HttpLink,
+} from "@apollo/client";
+
+const endpoint1 = new HttpLink({
+  uri: "https://api.hashnode.com/",
+});
+
+const endpoint2 = new HttpLink({
+  uri: "https://api.github.com/graphql",
+  headers: {
+    Authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
+  },
+});
 
 const client = new ApolloClient({
   ssrMode: true,
-  link: createHttpLink({
-    uri: "https://api.hashnode.com/",
-    credentials: "same-origin",
-    headers: {
-      Authorization: process.env.HASHNODE_AUTH,
-    },
-  }),
+  link: ApolloLink.split(
+    (operation) => operation.getContext().clientName === "endpoint2",
+    endpoint2,
+    endpoint1
+  ),
   cache: new InMemoryCache(),
 });
 
